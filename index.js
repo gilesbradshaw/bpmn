5b51cc82ee302e0fd7cbac19d61b88d21416a8bd
Giles <giles.bradshaw@sigyl.com>
2018-05-04 09:36:24 +0100

    very basic example

---
+++ index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const Bpmn = require('bpmn-engine');
+const EventEmitter = require('events').EventEmitter;
+
+const processXml = `
+<?xml version="1.0" encoding="UTF-8"?>
+<definitions xmlns="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <process id="theProcess" isExecutable="true">
+    <dataObjectReference id="inputFromUserRef" dataObjectRef="inputFromUser" />
+    <dataObject id="inputFromUser" />
+    <startEvent id="theStart" />
+    <userTask id="userTask">
+      <ioSpecification id="inputSpec">
+        <dataOutput id="userInput" />
+      </ioSpecification>
+      <dataOutputAssociation id="associatedWith" sourceRef="userInput" targetRef="inputFromUserRef" />
+    </userTask>
+    <endEvent id="theEnd" />
+    <sequenceFlow id="flow1" sourceRef="theStart" targetRef="userTask" />
+    <sequenceFlow id="flow2" sourceRef="userTask" targetRef="theEnd" />
+  </process>
+</definitions>`;
+
+const engine = new Bpmn.Engine({
+  name: 'listen example',
+  source: processXml
+});
+const listener = new EventEmitter();
+
+listener.once('wait-userTask', (task) => {
+  console.log({ task });
+  setTimeout(
+    () => task.signal({
+      sirname: 'Bradshaw'
+    }),
+    1000,
+  );
+});
+
+listener.on('taken', (flow) => {
+  console.log(`flow <${flow.id}> was taken`);
+});
+
+engine.once('end', (definition) => {
+  console.log(`User sirname is ${definition.variables.inputFromUser.sirname}`);
+});
+
+engine.execute({
+  listener: listener
+}, (err, instance) => {
+  if (err) throw err;
+});
